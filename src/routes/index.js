c86afa2bd1d37f82b2ee24ef32c2e1d937db9055
Grimaldus-Astartes <peregrinarosales97@gmail.com>
2023-04-01 12:04:11 -0600

    Crete the basic skelleton of our API

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+const { Router } = require('express')
+const router = Router();
+
+router.get("/", (req, res) => {
+    res.json({
+      title: "Hola mundo",
+    });
+  });
+
+module.exports = router;
